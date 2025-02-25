8ea04d45f14296ae15f9a3502c487c4985aaa69c
radim645 <micanekradim@seznam.cz>
2025-02-25 21:45:07 +0100

    boxy pro knihy

--- borrow.php
+++ borrow.php
@@ -3,6 +3,11 @@ require_once "layout/header.php";
 ?>
 
 
+
+
+
+
+
 <?php
 require_once "layout/footer.php";
 ?>
--- index.php
+++ index.php
@@ -24,32 +24,32 @@ require_once "layout/header.php";
 
 <h1>KNIHOVNA</h1>
 
+<?php
 
+$sql = "SELECT id, nazev, autor, datum_vydani, pocet_stran, nakladatelstvi, pocet_kusu FROM knihy";
 
+$sqlstat = mysqli_query($con, $sql);
 
-    <div class="window">
-        <h2>Kniha 1</h2>
-    </div>
+if ($sqlstat && mysqli_num_rows($sqlstat) > 0) {
+    echo "<div class='knihy_container'>";
+    while ($row = mysqli_fetch_assoc($sqlstat)) {
+        echo "<div class='knihy'>";          
+        echo "<h2>" . $row['nazev'] . "</h2>";
+        echo "<p>Autor: " . $row['autor'] . "</p>";
+        echo "<p>Datum vydání: " . $row['datum_vydani'] . "</p>";
+        echo "<p>Počet stran: " . $row['pocet_stran'] . "</p>";
+        echo "<p>Nakladatelství: " . $row['nakladatelstvi'] . "</p>";
+        echo "<p>Počet kusů skladem: " . $row['pocet_kusu'] . "</p>";
+        echo "<form action='index.php' method='POST'>";
+        echo "<input type='hidden' name='id_knihy' value='" . $row['id'] . "'>";
+        echo "<input type='submit' name='borrow_bk' value='Rezervovat knihu'>";
+        echo "</form>";
+        echo "</div>";
+    }
+    echo "</div>"; 
+}
 
-    <div class="window">
-        <h2>Kniha 2</h2>
-    </div>
-
-    <div class="window">
-        <h2>Kniha 3</h2>
-    </div>
-
-    <div class="window">
-        <h2>Kniha 4</h2>
-    </div>
-
-    <div class="window">
-        <h2>Kniha 5</h2>
-    </div>
-
-
-
-
+?>
 
 <?php
 require_once "layout/footer.php";
