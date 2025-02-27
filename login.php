38636572152f95ec20901392f7d17dc437511be4
radim645 <micanekradim@seznam.cz>
2025-02-27 22:35:02 +0100

    d

--- login.php
+++ login.php
@@ -1,27 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prihlaseni</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
 <?php
-
-
 require_once "layout/header.php";
-include "connect.php";
+?>
+<?php
+session_start();
+require 'db.php';
 ?>
 
-<h1>Přihlášení</h1>
+<div class="container">
 
-<title>Přihlášení</title>
-    
-<form action="login.php" method="post">
-<div class="box">
-    <label>Email:</label><br>
-    <input type="email" name="email" required><br>
-    <label>Heslo:</label><br>
-    <input type="password" name="password" required><br>    
-</div>
+<h3>Přihlášení</h3>
+
+<form method="POST">
+    <label for="username">Přihlašovací jméno:</label>
+    <input type="text" name="username" required>
+    </br>
+
+    <label for="password">Heslo:</label>
+    <input type="password" name="password" required>
+    </br>
+
     <input type="submit" name="login" value="Přihlásit se">
-</form>
 
-<div>
-Nemáte ještě účet?
-<a href="registrace.php">Zaregistrujte se</a>
+    </form>
+
+<p>Nemáte účet?<a href="reg.php">Registrujte se zde</a></p>
+
 </div>
 
 <?php
@@ -32,12 +45,12 @@ $invalid=0;
 
 if(isset($_POST['login'])){
 
-    $email = mysqli_real_escape_string($con, $_POST['email']); 
-    $password = md5(mysqli_real_escape_string($con, $_POST['password'])); 
+    $username = mysqli_real_escape_string($conn, $_POST['username']); 
+    $password = md5(mysqli_real_escape_string($conn, $_POST['password'])); 
 
-    $sql="select id, name, surname, email from `users` where email='$email' and password='$password'";
+    $sql="select id, username, name, surname, email, role, address from `users` where username='$username' and password='$password'";
 
-    $sqlstat=mysqli_query($con,$sql);
+    $sqlstat=mysqli_query($conn,$sql);
     if($sqlstat) {
         $num=mysqli_num_rows($sqlstat);
         if($num>0) {
@@ -46,12 +59,15 @@ if(isset($_POST['login'])){
             $row = mysqli_fetch_assoc($sqlstat); 
 
             $_SESSION['id'] = $row['id'];
+            $_SESSION['username'] = $row['username']; 
             $_SESSION['name'] = $row['name'];
             $_SESSION['surname'] = $row['surname'];
             $_SESSION['email'] = $row['email']; 
+            $_SESSION['role'] = $row['role'];
+            $_SESSION['address'] = $row['address'];  
             $_SESSION['logged_id'] = true;
 
-            header('location:ucet.php');
+            header('location:index.php');
 
         } else {
             $invalid=1;      
@@ -64,15 +80,16 @@ if(isset($_POST['login'])){
 <?php
 
 if($invalid){
-    echo '<script>alert("Špatné heslo, zkuste to znovu")</script>';
-}
-
-if($logged){
-    echo '<script>alert("Jste úspěšně přihlášeni")</script>';
+    echo '<script>alert("Špatně zadané heslo nebo přihlašovací jméno")</script>';
 }
 
 ?>
+
 
 <?php
 require_once "layout/footer.php";
 ?>
+
+    
+</body>
+</html>
